bf493320fe8bed490c29b43cd70f8b7d5b327c94
Artur Wronowski <arteqw@gmail.com>
2019-09-25 16:52:23 +0200

    auth

--- formio.js
+++ formio.js
@@ -1,11 +1,11 @@
-module.exports = function (RED) {
+module.exports = function(RED) {
   "use strict";
 
   function FormioConfig(node) {
     RED.nodes.createNode(this, node);
-    this.login = node.login;
-    this.password = node.password;
+    // this.login = node.login;
+    // this.password = node.password;
     this.project = node.project;
   }
   RED.nodes.registerType("formio", FormioConfig);
-}
+};
